6451e49277cce7b58923462fc355231016373af7
Tanner Moushey <tanner@missionlab.dev>
2022-06-10 10:19:30 -0500

    Location finder style updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@
 	const feather = require('feather-icons');
 	
 	feather.replace();
-	$(document).ready(() => feather.replace() );
+	
+	$(document).ready(() => {
+		feather.replace();
+		
+		document.body.style.setProperty('--cp-header-height', document.getElementById('masthead').offsetHeight + 'px');
+	} );
 	
 })(jQuery);
